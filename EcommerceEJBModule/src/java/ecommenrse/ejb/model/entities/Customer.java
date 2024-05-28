7c557e398be62e0e00e2c3f95d71c14525b0f08d
FowardPro <147938121+FowardPro@users.noreply.github.com>
2024-05-28 22:50:11 +0200

    Add files via upload

---
+++ EcommerceEJBModule/src/java/ecommenrse/ejb/model/entities/Customer.java
@@ -0,0 +1,63 @@
+
+package ecommenrse.ejb.model.entities;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String secondName;
+    @OneToOne(cascade = CascadeType.ALL)
+    private String email;
+    private String phoneNumber;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address streetAdress;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Cart cart;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Order order;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ecommenrse.ejb.model.entities.Customer[ id=" + id + " ]";
+    }
+    
+}
---
+++ EcommerceEJBModule/src/java/ecommenrse/ejb/model/entities/Order.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecommenrse.ejb.model.entities;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Foward
+ */
+@Entity
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Long userId;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Payment payment;
+    @OneToMany(cascade = CascadeType.ALL)
+    private Product products;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Order)) {
+            return false;
+        }
+        Order other = (Order) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ecommenrse.ejb.model.entities.Order[ id=" + id + " ]";
+    }
+    
+}
